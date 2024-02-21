4a11a8e51e2c7e30f34b0825fcdedb4b6053c6c6
ThiagoFTelles <atendimento.telles@gmail.com>
2024-02-21 10:44:12 -0300

    initial setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -7,5 +6,3 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <span>offline</span>
   </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
