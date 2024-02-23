a90a0d41a3bb5e0404b8e7f59c9d7b908ca03cd0
ThiagoFTelles <atendimento.telles@gmail.com>
2024-02-23 10:57:55 -0300

    feat: send message to server

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -7,11 +7,34 @@ export const connetToServer = () => {
 }
 
 const addListeners = ( socket: Socket ) => {
-    const serverStatusLabel = document.querySelector('#server-status')!
+    const serverStatusLabel = document.querySelector<HTMLSpanElement>('#server-status')!
+    const clientsList = document.querySelector<HTMLUListElement>('#clients-list')!
+    const messageForm = document.querySelector<HTMLFormElement>('#message-form')!
+    const messageInput = document.querySelector<HTMLInputElement>('#message-input')!
+
     socket.on('connect', () => {
         serverStatusLabel.innerHTML = 'connected'
     })
     socket.on('disconnect', () => {
         serverStatusLabel.innerHTML = 'disconnected'
+    })
+    socket.on('clients-updated', ( clients: string[] ) => {
+        let clientsHtml = ''
+        clients.forEach( clientId => {
+            clientsHtml += `
+                <li>${ clientId }</li>
+            `
+        })
+        clientsList.innerHTML = clientsHtml
+    })
+
+    messageForm.addEventListener('submit', ( event ) => {
+        event.preventDefault()
+        if( messageInput.value.trim().length <= 0 ) return
+        socket.emit('message-from-client', {
+            id:'Eu!',
+            message:messageInput.value
+        })
+        messageInput.value = ''
     })
 }
