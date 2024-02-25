4b060e1c4792176799eb40b14d9b02fd1ddab151
ThiagoFTelles <atendimento.telles@gmail.com>
2024-02-25 15:43:38 -0300

    feat: send token to server

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Websocket - Client</h1>
+    <h2>Websocket - Client</h2>
+    <input id="jwt-token" placeholder="Json Web Token" />
+    <button id="btn-connect">Connect</button>
+    <br/>
     <span id="server-status">offline</span>
     <ul id="clients-list"></ul>
     <form id="message-form">
@@ -13,4 +16,10 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <ul id="messages-list"></ul>
   </div>
 `
-connetToServer()
+const jwtToken = document.querySelector<HTMLInputElement>('#jwt-token')!
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect')!
+
+btnConnect.addEventListener('click', () => {
+  if( jwtToken.value.trim().length <= 0 ) return alert('Enter a valid JWT')
+  connetToServer( jwtToken.value.trim() )
+})
